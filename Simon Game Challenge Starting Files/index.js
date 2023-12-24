247f61489431eb84f2446eaf69090e32480ee383
Ismat-Samadov <ismetsemedov@gmail.com>
2023-12-24 07:34:53 +0400

    check

--- Simon Game Challenge Starting Files/index.js
+++ Simon Game Challenge Starting Files/index.js
@@ -15,12 +15,20 @@ function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 3);
   var randomChosenColour = buttonColours[randomNumber];
   
-  gamePattern.push(randomChosenColour);
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-  
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-  audio.play();
+   flash(randomChosenColour);
+  playSound(randomChosenColour);
 }
 
 
+
+
+playSound(name_of_sound){ 
+  var audio = new Audio("sounds/" + name_of_sound + ".mp3");
+  audio.play();
+}
+
+flash(name_of_color){
+ gamePattern.push(name_of_color);
+  $("#" + name_of_color).fadeIn(100).fadeOut(100).fadeIn(100);
+}
 
