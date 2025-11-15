cbfd55852af7016e1190ce39a61da3e572451a90
Ismat-Samadov <ismetsemedov@gmail.com>
2025-11-15 19:23:58 +0400

    init

--- js/whosPaying.js
+++
@@ -1,7 +0,0 @@
-function whosPaying(names) {
-    var numOfPeople = names.length;
-    var randomIndex = Math.floor(Math.random() * numOfPeople);
-    var selectedPerson = names[randomIndex];
-    return selectedPerson + " is going to buy lunch today!";
-}
-
